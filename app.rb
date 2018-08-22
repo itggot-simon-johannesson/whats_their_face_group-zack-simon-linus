6af2a09a26bbc2a609c2ce3ec91db1f178b059fb
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-08-22 15:29:58 +0200

    syntax error fix for gaurd statement

--- app.rb
+++ app.rb
@@ -7,14 +7,14 @@ class App < Sinatra::Base
   end
 
   get '/guess_game/:class' do
-    session[:person_id] = Person.first(:offset => rand(Person.count), :class = params[:class]).id
-    puts "random person is #{session[:person_id]}" 
+    session[:person_id] = rand(Person.count)
+    puts "random person is #{session[:person_id]}"
     session[:alternatives] = [0, 2, 3, 4]
   	slim :guess_game
   end
 
   get '/answer/:guess_id' do
-    redirect_to '/' unless session[:person_id]
+    redirect '/' unless session[:person_id]
     the_person_guessed = Person.get(params[:guess_id])
     the_person_it_is = Person.get(session[:person_id])
     puts the_person_guessed.name
