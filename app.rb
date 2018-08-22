f5f41327a60e10097a38f034368d95462c4b68a9
Simon Johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-08-22 16:10:44 +0200

    vrt ej

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ class App < Sinatra::Base
   end
 
   get '/guess_game/:class' do
-<<<<<<< HEAD
     
     p session[:person_id] = Person.all(:class => params[:class].upcase)[]
   
@@ -15,7 +14,7 @@ class App < Sinatra::Base
 
 
     "random person is #{session[:person_id]}"
->>>>>>> 6af2a09a26bbc2a609c2ce3ec91db1f178b059fb
+
     session[:alternatives] = [0, 2, 3, 4]
   	slim :guess_game
   end
