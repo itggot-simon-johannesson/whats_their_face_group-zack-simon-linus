8dba04b6f95d07683935784d962562f908dae3de
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-08-23 13:31:42 +0200

    fixed case for name

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,27 @@
+Vue.component(`card-component`, {
+    props: {
+        person_name: {
+            type: String
+        },
+        person_image: {
+            type: String
+        },
+        do_show_image: {
+            type: Boolean
+        }
+    },
+    data() {
+        return {
+            is_card_shown: false
+        }
+    },
+    methods: {
+        flipCard() {
+            this.is_card_shown 
+        }
+    }
+});
+
 var app = new Vue({
     el: '#app',
     data: {
