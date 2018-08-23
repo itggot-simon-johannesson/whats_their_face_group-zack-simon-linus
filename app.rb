dfcb5379289965c363dcd266a6ba9c712e71fc1f
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-08-23 09:19:00 +0200

    changed person selection

--- app.rb
+++ app.rb
@@ -9,12 +9,18 @@ class App < Sinatra::Base
 
   get '/guess_game/:class' do
     
-    p session[:person_id] = Person.all(:class => params[:class].upcase)[]
+    #p session[:person_id] = Person.all(:class => params[:class].upcase)[]
+
+    person_query_result = repository(:default).adapter.select('SELECT id FROM people WHERE class LIKE ? ORDER BY random() LIMIT 1;', params[:class])
+
+    redirect '/' unless person_query_result.length > 0
+    
+    session[:person_id] = person_query_result[0]
   
     #session[:person_id] = Person.first(:offset => rand(Person.count), :class = params[:class]).id
 
 
-    "random person is #{session[:person_id]}"
+    puts "random person is #{session[:person_id]}"
 
     session[:alternatives] = [0, 2, 3, 4]
   	slim :guess_game
