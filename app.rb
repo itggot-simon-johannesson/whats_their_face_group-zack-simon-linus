38c9375e9a83193d6c376d585cbf1570d734cd3a
Simon Johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-08-23 11:19:32 +0200

    la till guess game

--- app.rb
+++ app.rb
@@ -8,17 +8,49 @@ class App < Sinatra::Base
   end
 
   get '/guess_game/:class' do
-    
-    #p session[:person_id] = Person.all(:class => params[:class].upcase)[]
 
-    person_query_result = repository(:default).adapter.select('SELECT id FROM people WHERE class LIKE ? ORDER BY random() LIMIT 1;', params[:class])
+  session[:guessed_correct] = []
 
-    redirect '/' unless person_query_result.length > 0
-    
-    session[:person_id] = person_query_result[0]
-  
-    #session[:person_id] = Person.first(:offset => rand(Person.count), :class = params[:class]).id
+  one_not_guessed_correct = false
+  none_valid_answers = []
 
+  while one_not_guessed_correct == false
+    answers = repository(:default).adapter.select('SELECT id FROM people WHERE class LIKE ? ORDER BY random() LIMIT 4;', params[:class])
+
+    if session[:guessed_correct].size > 0
+      for person in session[:guessed_correct]
+        if answers.include?(person)
+          p "hej"
+            none_valid_answers << person
+        else
+          one_not_guessed_correct = true
+        end
+      end
+    else
+      one_not_guessed_correct = true
+    end
+
+  end
+
+  if none_valid_answers.size > 0
+    session[:person_id] = false
+    while session[:person_id] == false
+      for none_valid_answer in none_valid_answers
+        p "hej"
+        answer = answers.sample
+        session[:person_id] = answer unless answer == none_valid_answer
+      end
+    end
+  else
+    session[:person_id] = answers.sample
+  end
+
+  p none_valid_answers
+  p answers
+  p session[:guessed_correct]
+
+
+    #redirect '/' unless person_query_result.length > 0
 
     puts "random person is #{session[:person_id]}"
 
